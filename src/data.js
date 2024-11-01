677c6cd2bd763bdfe0b756ba0221bcc181870fb1
Alsuee <nurlygayanovaa@bk.ru>
2024-11-01 14:42:17 +0300

    crez2

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const products = [
+    {
+      id: 1,
+      name: "Толстовка с капюшоном",
+      image: "https://i.pinimg.com/564x/d4/6b/88/d46b88c53fe99019a75b64587b0b4cc3.jpg",
+      price: 2500,
+      size: "M",
+      quantity: 5,
+      category: "Классические"
+    },
+    {
+      id: 2,
+      name: "Толстовка оверсайз",
+      image: "https://i.pinimg.com/564x/80/41/00/8041001de0c128819cc523040ea5748d.jpg",
+      price: 3000,
+      size: "L",
+      quantity: 10,
+      category: "Оверсайз"
+    },
+    {
+      id: 3,
+      name: "Толстовка с принтом",
+      image: "https://i.pinimg.com/564x/e3/12/37/e31237bdf55b6762cd52a0b7271ec61d.jpg",
+      price: 3500,
+      size: "S",
+      quantity: 0,
+      category: "С принтом"
+    },
+    {
+      id: 4,
+      name: "Утепленная толстовка",
+      image: "https://i.pinimg.com/736x/c5/a5/95/c5a595b480c9a334874e6cd53103eb5b.jpg",
+      price: 4000,
+      size: "XL",
+      quantity: 3,
+      category: "Утепленные"
+    },
+    {
+      id: 5,
+      name: "Спортивная толстовка",
+      image: "https://i.pinimg.com/564x/39/55/f0/3955f097d9c0e98d4701850674ce3f2b.jpg",
+      price: 3200,
+      size: "M",
+      quantity: 8,
+      category: "Спортивные"
+    }
+  ];
